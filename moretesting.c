02ba2dc546c7680eb56a25b067c4e6d6a607128e
Ayan Biswas <ayan.biswas953@gmail.com>
2019-11-07 23:17:52 +0530

    More or less ready for submission

--- moretesting.c
+++ moretesting.c
@@ -6,13 +6,14 @@
 
 int main(){
 	int pid, wtime, rtime;
+	int trtime=0,twtime=0;
 	printf(1,"Commencing Test:\n");
 	volatile int x, y, j, k;
 	for(int i=0; i < NFORK; i++){
 		switch(pid=fork()){
 			case -1:printf(1,"Unable to fork. i=%d\n", i);
 			break;
-			case 0: for(k=0; k < 100; k++)
+			case 0: for(k=0; k < 10; k++)
 						for(j=0; j < 1000000; j++){
 							y+= (x*x*y+x)%100007;
 						}
@@ -26,6 +27,11 @@ int main(){
 			printf(1,"\nWaitx failed");
 		//else
 			//printf(1,"\nProcess = %d Run time = %d, Wait time = %d\n", i, rtime, wtime);
+		trtime+=rtime;
+		twtime+=wtime;
 	}
+	trtime/=NFORK;
+	twtime/=NFORK;
+	printf(1,"Average Run Time = %d, Wait Time = %d\n",trtime,twtime );
 	exit();
 }
